01bdbdb359227abf7031438b30075ed9e535c61d
ahmedduke12 <ahmedduke1@gmail.com>
2026-01-11 14:31:50 +0300

    Add .js extensions to all relative imports in routers.ts

--- server/routers.ts
+++ server/routers.ts
@@ -1,10 +1,10 @@
 import { COOKIE_NAME } from "../shared/const.js";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-import { getSessionCookieOptions } from "./_core/cookies";
-import { systemRouter } from "./_core/systemRouter";
-import { publicProcedure, protectedProcedure, router } from "./_core/trpc";
-import * as db from "./db";
+import { getSessionCookieOptions } from "./_core/cookies.js";
+import { systemRouter } from "./_core/systemRouter.js";
+import { publicProcedure, protectedProcedure, router } from "./_core/trpc.js";
+import * as db from "./db.js";
 
 // ============================================================================
 // COMPLIANCE ENGINE
